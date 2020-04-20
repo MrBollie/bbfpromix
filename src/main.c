f8ac14e5d321a2eee504557b43507a83f97bd50a
MrBollie <github@ca9.eu>
2020-04-20 19:56:52 +0200

    Added GPL info

--- src/main.c
+++ src/main.c
@@ -1,3 +1,23 @@
+/*  bbfpromix
+ *
+ *  Copyright 2020 Thomas Ebeling <penguins@bollie.de>
+ *
+ *  This file is part of bbfpromix.
+ *
+ *  Foobar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Foobar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with bbfpromix.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <gtk/gtk.h>
