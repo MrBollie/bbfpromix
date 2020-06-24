5d89b936839d6988def4de22d268ab0346aeac57
MrBollie <github@ca9.eu>
2020-06-24 18:57:20 +0200

    Removed unwanted debug output and changed file headers

--- src/main.c
+++ src/main.c
@@ -4,12 +4,12 @@
  *
  *  This file is part of bbfpromix.
  *
- *  Foobar is free software: you can redistribute it and/or modify
+ *  bbfpromix is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Foobar is distributed in the hope that it will be useful,
+ *  bbfpromix is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
@@ -262,7 +262,6 @@ static void activate(GtkApplication *app, gpointer *user_data) {
 int main(int argc, char** argv) {
     GtkApplication *app;
     bbf_app_data_t app_data;
-    printf("WHAT");
     app_data.mixer = NULL;
     app_data.no_signals = false;
     int status;
